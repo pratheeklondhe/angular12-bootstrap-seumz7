24232566779865866a507615b78977b9ff71a197
pratheeklondhe <pratheeklondhe2014@gmail.com>
2021-12-20 10:35:45 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,16 +11,20 @@ export class AppComponent {
 
   ngOnInit() {
     this.formdata = new FormGroup({
-      emailid: new FormControl('angular@gmail.com', Validators.required),
-      fname: new FormControl('alen', Validators.required),
+      emailid: new FormControl('angular@gmail.com', [
+        Validators.required,
+        Validators.email,
+      ]),
+      fname: new FormControl('', Validators.required),
       lname: new FormControl('john', Validators.required),
       addr: new FormControl('newyork', Validators.required),
-      mobile: new FormControl('India', Validators.required),
+      mobile: new FormControl('8139918437', Validators.required),
       role: new FormControl('Admin', Validators.required),
       department: new FormControl('Finance', Validators.required),
       country: new FormControl('India', Validators.required),
     });
   }
+
   onClickSubmit(data: any) {
     console.table(data);
   }
